e98ec9c07f3e4da08ea8634db9fb33364d9ec151
Marc Logemann <marc@okaycloud.de>
2020-04-29 22:46:40 +0200

    first commit

---
+++ src/test/java/de/okaycloud/HelloWorldWebapp/HelloWorldWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.okaycloud.HelloWorldWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
