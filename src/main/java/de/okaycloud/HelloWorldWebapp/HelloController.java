3987b1b8fadf65d4908bfe78de61f216baf51ff6
Marc Logemann <marc@okaycloud.de>
2020-04-29 23:33:07 +0200

    sdfsdfdsf

--- src/main/java/de/okaycloud/HelloWorldWebapp/HelloController.java
+++ src/main/java/de/okaycloud/HelloWorldWebapp/HelloController.java
@@ -2,13 +2,18 @@ package de.okaycloud.HelloWorldWebapp;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Random; 
+
 
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Die Helga, die mag ich sehr.!";
+		Random rand = new Random(); 
+		int rand_int1 = rand.nextInt(100); 
+		return "Die Helga ("+rand_int1+ " Jahre), die mag ich sehr.!";
 	}
 
 }
